74fde40ddfa1dbd4ce7ed20d6d07aee448e4c4a5
Vlad Shcherban <vlad@coronalabs.com>
2020-04-13 23:52:08 -0400

    Adding binaries

---
+++ plugins/2020.3569/iphone/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'APDAdColonyAdapter', },
+		frameworks = { 'AdColony', },
+		frameworksOptional = { 'EventKit', 'WatchConnectivity', 'Social', },
+	},
+}
+
+return metadata
